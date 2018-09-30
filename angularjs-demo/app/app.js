606481baa112d405f05d51a409d15a53a9d3ea06
Yahima Duarte <yahima.duarte@iristrace.com>
2018-09-30 22:45:56 +0200

    docker config for prod

--- angularjs-demo/app/app.js
+++ angularjs-demo/app/app.js
@@ -2,13 +2,13 @@
 
 // Declare app level module which depends on views, and components
 var App = angular.module('myApp', [
-  'ngRoute',
-  'myApp.dogs',
-  'myApp.cats',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/dogs'});
+    'ngRoute',
+    'myApp.dogs',
+    'myApp.cats',
+    'myApp.version'
+]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    // $locationProvider.hashPrefix('!');
+    $locationProvider.html5Mode(false);
+    $locationProvider.hashPrefix('');
+    $routeProvider.otherwise({redirectTo: '/dogs'});
 }]);
