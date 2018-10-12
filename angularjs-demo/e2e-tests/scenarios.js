e8c11e0162f658a97948b447e9c433db174c7abc
Yahima Duarte <yahima.duarte@iristrace.com>
2018-10-12 11:22:32 +0200

    fix tests and refresh behaviour

--- angularjs-demo/e2e-tests/scenarios.js
+++ angularjs-demo/e2e-tests/scenarios.js
@@ -2,41 +2,34 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function() {
-
-
-  it('should automatically redirect to /dogs when location hash/fragment is empty', function() {
-    browser.get('index.html');
-    expect(browser.getCurrentUrl()).toMatch("/dogs");
-  });
-
-
-  describe('dogs', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#!/dogs');
+describe('my app', function () {
+    
+    it('should automatically redirect to /dogs when location hash/fragment is empty', function () {
+        browser.get('index.html');
+        expect(browser.getCurrentUrl()).toMatch("/dogs");
     });
 
+    describe('dogs', function () {
 
-    it('should render dogs when user navigates to /dogs', function() {
-      expect(element.all(by.css('[ng-view] h1')).first().getText()).
-        toMatch(/Dogs/);
+        beforeEach(function () {
+            browser.get('/dogs');
+        });
+
+        it('should render dogs when user navigates to /dogs', function () {
+            expect(element.all(by.css('[ui-view] h1')).first().getText()).toMatch(/Dogs/);
+        });
+
     });
 
-  });
+    describe('help', function () {
 
+        beforeEach(function () {
+            browser.get('/help');
+        });
 
-  describe('cats', function() {
+        it('should render help page when user navigates to /help', function () {
+            expect(element.all(by.css('[ui-view] h1')).first().getText()).toMatch(/Help/);
+        });
 
-    beforeEach(function() {
-      browser.get('index.html#!/cats');
     });
-
-
-    it('should render cats when user navigates to /cats', function() {
-      expect(element.all(by.css('[ng-view] h1')).first().getText()).
-        toMatch(/Cats/);
-    });
-
-  });
 });
